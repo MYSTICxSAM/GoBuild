324d67a21c6607ddcd660a34a6cc757a4929dfbe
092vk <vk092kumar@gmail.com>
2025-08-11 12:50:43 +0530

    fix:fixed phone no

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -100,8 +100,9 @@ const Footer: React.FC = () => {
               </li>
               <li className="flex items-center">
                 <Phone className="w-5 h-5 mr-3 text-accent" />
-                <Link to="tel:+91889931011" className="text-gray-400 hover:text-white transition-colors">+91 889931011</Link>
+                <Link to="tel:+91889931011" className="text-gray-400 hover:text-white transition-colors">+91 8899310111</Link>
                 <Link to="tel:+917051514790" className="text-gray-400 hover:text-white transition-colors">+91 7051514790</Link>
+                <Link to="tel:+919596133638" className="text-gray-400 hover:text-white transition-colors">+91 9596133638</Link>
               </li>
               <li className="flex items-center">
                 <Mail className="w-5 h-5 mr-3 text-accent" />
