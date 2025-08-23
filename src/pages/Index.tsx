f957f921b13aa2fd83e7fcaa83b7a3e005ee2741
092vk <vk092kumar@gmail.com>
2025-08-23 17:16:50 +0530

    added FAQ , and other components

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,12 +2,15 @@
 import React, { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import WorkerCategories from "../components/WorkerCategories";
 import ServiceCategories from "../components/ServiceCategories";
 import HowItWorks from "../components/HowItWorks";
 import FeaturedProfessionals from "../components/FeaturedProfessionals";
 import Testimonials from "../components/Testimonials";
 import Footer from "../components/Footer";
 import HeroForm from "@/components/HeroForm";
+import FAQ from "../components/FAQ";
+// import { Banner } from "@/components/Banner";
 
 const Index: React.FC = () => {
   // Smooth scroll functionality
@@ -42,9 +45,12 @@ const Index: React.FC = () => {
     <div className="min-h-screen bg-background">
       <Navbar />
       <Hero />
+      {/* <Banner/> */}
       <HeroForm/>
-      <ServiceCategories />
+      <WorkerCategories />
+      {/* <ServiceCategories /> */}
       <HowItWorks />
+      <FAQ />
       {/* <FeaturedProfessionals /> */}
       {/* <Testimonials /> */}
       <Footer />
